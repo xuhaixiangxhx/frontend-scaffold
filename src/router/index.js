8deb6fb7b4236543f82a645993b90cd60e36405f
xuhaixiang <xuhaixiangxhx@163.com>
2024-04-22 12:46:59 +0800

    路由守卫处理登录逻辑

--- src/router/index.js
+++ src/router/index.js
@@ -20,4 +20,35 @@ const router = createRouter({
     routes
 })
 
+// 路由前置守卫，处理登录逻辑
+/*
+    1 访问/login，没有携带token
+    2 访问/login，携带了token
+    3 访问非/login，没有携带token
+    4 访问非/login，携带了token
+*/
+router.beforeEach(
+    (to, from, next) => {
+        // 获取访问路径
+        const targetPath = to.path
+        // 判断访问路径是否为/login
+        const isLoginPath = targetPath.indexOf('/login')
+        // 获取token
+        const token = localStorage.getItem('Authorization')
+        if(isLoginPath == 0 && token != null ){
+            // 登录页面，且携带token
+            next('/')
+        }else if(isLoginPath == 0 && token == null){
+            // 登录页面，未携带token
+            next()
+        }else if (token != null){
+            // 非登录页面，携带token
+            next()
+        }else {
+            // 非登录页面，未携带token
+            next('/login')
+        }
+    }
+)
+
 export default router
