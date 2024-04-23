11a02043128b0cf8eae9a63dc5c9ab4d8b7010da
xuhaixiang <xuhaixiangxhx@163.com>
2024-04-23 10:19:02 +0800

    完成Logo信息展示

--- src/views/layout/components/Aside.vue
+++ src/views/layout/components/Aside.vue
@@ -1,9 +1,30 @@
 <script setup>
+import { Ship } from '@element-plus/icons-vue';
 </script>
 
 <template>
-    <el-aside width="200px">Aside</el-aside>
+    <el-aside>
+        <div class="aside-logo">
+            <el-button text><el-icon><Ship /></el-icon>后台管理</el-button>
+        </div>
+    </el-aside>
 </template>
 
 <style scoped>
+.el-aside {
+    width: 240px;
+    border-right: 1px solid #cccccc;
+}
+.aside-logo {
+    height: 50px;
+}
+.aside-logo .el-button {
+    width: 100%;
+    height: 100%;
+    font-size: 20px;
+    outline: none;
+}
+.el-icon {
+    margin-right : 5px;
+}
 </style>
