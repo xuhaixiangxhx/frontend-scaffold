d2e4b7b70195b0625a551434b12d0d736549e4c0
xuhaixiang <xuhaixiangxhx@163.com>
2024-04-23 20:14:06 +0800

    使用阿里开源的iconfont定义菜单图标，二次提交

--- src/views/layout/components/Aside.vue
+++ src/views/layout/components/Aside.vue
@@ -16,6 +16,7 @@ import { MENU_CONFIG } from '../../../config/menu.js';
         <el-menu default-active="2" class="el-menu-vertical-demo" router>
             <el-sub-menu v-for="menu in MENU_CONFIG" :key="menu.index" :index="menu.index">
                 <template #title>
+                    <!-- 使用iconfont -->
                     <el-icon><span :class="menu.icon"></span></el-icon>
                     <span>{{ menu.title }}</span>
                 </template>                
@@ -23,14 +24,19 @@ import { MENU_CONFIG } from '../../../config/menu.js';
                 <template v-if="menu.subMenu">
                     <el-sub-menu v-for="subMenu in menu.subMenu" :key="subMenu.index" :index="subMenu.index">
                         <template #title>
+                            <!-- 使用iconfont -->
                             <el-icon><span :class="subMenu.icon"></span></el-icon>
                             <span>{{ subMenu.title }}</span>
                         </template>
-                        <el-menu-item v-for="item in subMenu.items" :key="item.index" :index="item.index">{{ item.title }}</el-menu-item>
+                        <el-menu-item v-for="item in subMenu.items" :key="item.index" :index="item.index">
+                            {{ item.title }}
+                        </el-menu-item>
                     </el-sub-menu>
                 </template>
                 <template v-else>
-                    <el-menu-item v-for="item in menu.items" :key="item.index" :index="item.index">{{ item.title }}</el-menu-item>
+                    <el-menu-item v-for="item in menu.items" :key="item.index" :index="item.index">
+                        {{ item.title }}
+                    </el-menu-item>
                 </template>
             </el-sub-menu>
         </el-menu>
